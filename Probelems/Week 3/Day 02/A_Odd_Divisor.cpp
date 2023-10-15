039a5b07ce7c14034100ba15eafbb3a9fb97f49b
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-10-15 17:19:42 +0600

    day-2

---
+++ Probelems/Week 3/Day 02/A_Odd_Divisor.cpp
@@ -0,0 +1,43 @@
+//....................................<In the name of Allah>...............................//
+
+//.................................<Bismillahir Rahmanir Rahim>...................................//
+// Author : Rakibul Hassan
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned ll
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+#define gcd(aaa, aaaa) __gcd((aaa), (aaaa));
+#define lcm(aaa, aaaa) (abs((aaa) * (aaaa)) / gcd(aaa, aaaa))
+#define TestCase() \
+    int tc;        \
+    cin >> tc;     \
+    while (tc--)
+#define fasterIO()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0)
+using namespace std;
+
+void solve()
+{
+    long long n;
+    cin >> n;
+    if ((n & (n - 1)) == 0)
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
+}
+int main()
+{
+    fasterIO();
+    TestCase()
+        solve();
+
+    return 0;
+}
---
+++ Probelems/Week 3/Day 02/A_Polycarp_and_the_Day_of_Pi.cpp
@@ -0,0 +1,56 @@
+//....................................<In the name of Allah>...............................//
+
+//.................................<Bismillahir Rahmanir Rahim>...................................//
+// Author : Rakibul Hassan
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned ll
+#define vi vector<int>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl '\n'
+#define gcd(aaa, aaaa) __gcd((aaa), (aaaa));
+#define lcm(aaa, aaaa) (abs((aaa) * (aaaa)) / gcd(aaa, aaaa))
+#define TestCase() \
+    int tc;        \
+    cin >> tc;     \
+    while (tc--)
+#define fasterIO()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0)
+
+using namespace std;
+
+string pi = "3141592653589793238462643383279";
+void solve()
+{
+    string n;
+    cin >> n;
+
+    int count = 0;
+    for (int i = 0; i < n.length(); i++)
+    {
+        if (n[i] == pi[i])
+        {
+            count++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    cout << count << endl;
+}
+int main()
+{
+    fasterIO();
+    TestCase()
+        solve();
+
+    return 0;
+}
