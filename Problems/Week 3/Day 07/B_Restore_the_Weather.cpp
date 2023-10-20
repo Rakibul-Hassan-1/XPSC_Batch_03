bc6191f3b99fa4632e40406eedaea1767a59f2e5
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-10-20 10:06:39 +0600

    day-7

---
+++ Problems/Week 3/Day 07/B_Restore_the_Weather.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define vi vector<int>
+#define pb push_back
+#define ff first
+#define ss second
+#define endl '\n'
+#define TestCase() \
+    int tc;        \
+    cin >> tc;     \
+    while (tc--)
+#define fasterIO()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0)
+using namespace std;
+
+void solve()
+{
+    
+}
+int main()
+{
+    fasterIO();
+    TestCase()
+        solve();
+
+    return 0;
+}
