56816104e7f95733787b2c2ef76f23a4d807cda7
RakibulHassan29052000 <rakobulsagor@gamil.com>
2023-11-08 20:22:15 +0600

    day-4

---
+++ Problems/Week 6/Day - 04/Oneful_Pairs.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int a, b;
+    cin >> a >> b;
+    if ((a + b + (a * b) == 111))
+        cout << "yes" << endl;
+
+    else
+        cout << "no" << endl;
+
+    return 0;
+}
