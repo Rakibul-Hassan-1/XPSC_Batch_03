cd4048494b2a42818cb5aa4f4140a9ab225193cf
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2023-12-30 19:12:34 +0600

    previous

---
+++ Problems/Week 12/Previous/A_Hard_Way.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define endl '\n'
+#define fasterIO()                 \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0)
+using namespace std;
+
+vector<int> getDivisors(int n)
+{
+    vector<int> d;
+    for (int i = 1; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            d.push_back(i);
+            if (i != (n / i))
+            {
+                d.push_back(n / i);
+            }
+        }
+    }
+    return d;
+}
+
+void solve()
+{
+    int a, b, c, d, e, f, ans = 0;
+    cin >> b >> a >> d >> c >> f >> e;
+    if (a == c && e < a)
+        ans = abs(b - d);
+    else if (c == e && a < c)
+        ans = abs(d - f);
+    else if (a == e && c < a)
+        ans = abs(b - f);
+    cout << ans << endl;
+}
+int main()
+{
+    fasterIO();
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
